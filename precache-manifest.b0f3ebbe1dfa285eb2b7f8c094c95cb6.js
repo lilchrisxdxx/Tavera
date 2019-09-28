d07d19255e6ee7646d7ce315880fa86558cc36a8
Christian <lilchrisxdxx@gmail.com>
2019-09-28 06:45:01 -0700

    Updates

---
+++ precache-manifest.b0f3ebbe1dfa285eb2b7f8c094c95cb6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40519a1ffcd61f2a37d14e2dfe1750f",
+    "url": "/Tavera/index.html"
+  },
+  {
+    "revision": "d506c969b71e09ba29f6",
+    "url": "/Tavera/static/css/main.2b74f2bb.chunk.css"
+  },
+  {
+    "revision": "70ed9c59dd6fb5933a96",
+    "url": "/Tavera/static/js/2.34b7421c.chunk.js"
+  },
+  {
+    "revision": "d506c969b71e09ba29f6",
+    "url": "/Tavera/static/js/main.0b4eff7a.chunk.js"
+  },
+  {
+    "revision": "96b9c94749686b9fca67",
+    "url": "/Tavera/static/js/runtime-main.fbf9d19e.js"
+  }
+]);
